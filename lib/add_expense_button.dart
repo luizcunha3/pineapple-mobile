e7f836c32b9db09c86f3b4c9dde21a099cf63f2d
Luiz <luiz.filho@prestador.com.br>
2020-03-25 14:27:36 -0300

    testes

---
+++ lib/add_expense_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class AddExpenseButton extends StatelessWidget {
+
+  AddExpenseButton({@required this.onPressed});
+
+  final GestureTapCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return RawMaterialButton(
+      fillColor: Colors.yellow,
+      splashColor: Colors.yellowAccent,
+      onPressed: onPressed,
+      shape: const StadiumBorder(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+            vertical: 8.0,
+            horizontal: 20.0,
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: const <Widget>[
+            Icon(
+              Icons.explore,
+              color: Colors.teal
+            ),
+            SizedBox(width: 8.0),
+            Text(
+              "Expense",
+              style: TextStyle(color: Colors.black),
+            ),
+          ],
+        ),
+      ),
+
+    );
+  }
+}
